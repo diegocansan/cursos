da6d5ce34cc7913ff90f388baf1a430cede8eb22
Diego Cansan <diego.cansan@gmail.com>
2024-11-19 15:49:43 -0300

    atualizando o endereco dos dados

--- js-developer-portfolio-main/assets/js/api.js
+++ js-developer-portfolio-main/assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/diegocansan/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/diegocansan/cursos/refs/heads/main/js-developer-portfolio-main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
